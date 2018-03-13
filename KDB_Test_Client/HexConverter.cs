5cb70e619dc5b24689cbf5b1029f617fab9f1a4b
dejung <dejung@adt.co.kr>
2018-03-13 11:14:03 +0900

    원격 클라이언트 수정

---
+++ KDB_Test_Client/HexConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Kdb.Util
+{
+    public static class HexConverter
+    {
+        public static byte[] ToHexBytes(this string message)
+        {
+            return Enumerable.Range(0, message.Length)
+                    .Where(x => x % 2 == 0)
+                    .Select(x => Convert.ToByte(message.Substring(x, 2), 16))
+                    .ToArray();
+        }
+        public static string ToString(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+                hex.AppendFormat("{0:x2}", b);
+            return hex.ToString();
+        }
+
+
+    }
+}
--- KDB_Test_Client/Program.cs
+++ KDB_Test_Client/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using Kdb.Util;
 
 namespace KDB_Test_Client
 {
@@ -10,23 +11,38 @@ namespace KDB_Test_Client
     {
         static void Main(string[] args)
         {
-            Socket sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("192.168.15.2"), 48484);
+            IPEndPoint serverAddress = new IPEndPoint(IPAddress.Parse("192.168.15.2"), 48484);
+            Socket sck = new Socket(serverAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
-
-            /*
-             719 장비 붙인 후에 프로토콜에 나와있는 사이즈대로 KDB 프로젝트 필터 렝스 수정해서 받아보고 
-             요청 안받아지면 팀장님한테 문서 정확도 물어보기
-             이것도 안되면 위아래로 +3정도까지 테스트 한 후에 문서랑 비교해보기
-
-             되면 커맨드 하나하나에 대하여 작성
-             다음주 수요일내로 커맨드에 대한 처리기반이 만들어져야 그걸 wcf라는 프레임웤에 어떻게 던져줄지 고민할 시간이 생김
-             */
-            
+            //16
+            var Alive =   "200d001030303138376265663238353430303030";
+            //39
+            var Access =  "2008002330303138376265663263623030303030011801181613000000000000000000FF008300";
+            var Access2 = "2018002330303138376265663235666430303030011801181550545CEE1A2E00000000FF000101";
+            //47자리
+            var Event =   "2008002B30303138376265663439333530303030021801190030153233343520313820453332322030302043303030";
+            var Event2 =  "2018002B30303138376265663235666430303030021801181502283233343520313820453131302030302043303033";
 
             try
             {
-                sck.Connect(localEndPoint);
+                sck.Connect(serverAddress);
+                Console.WriteLine("소켓 연결");
+
+                var socketStream = new NetworkStream(sck);
+                var reader = new StreamReader(socketStream, Encoding.Default, true);
+                var writer = new StreamWriter(socketStream, Encoding.Default, 1024*8);
+
+                //Byte[]로 쏴주는게 아니라  string
+
+                var byteData = HexConverter.ToHexBytes(Event);
+                var stringData = HexConverter.ToString(byteData);
+                writer.Write(stringData);
+                writer.Flush();
+                Console.WriteLine("Alilve Check Protocol Send");
+
+                var readBuffer = reader.ReadLine();
+                Console.WriteLine("Read : "+readBuffer);
+
             }
             catch
             {
@@ -34,11 +50,6 @@ namespace KDB_Test_Client
                 Main(args);
             }
 
-            Console.Write("Enter Text: ");
-            string text = Console.ReadLine();
-            byte[] data = Encoding.UTF8.GetBytes(text);
-
-            sck.Send(data);
             Console.Write("Data Sent!\r\n");
             Console.Write("Press any key To continue...");
             Console.Read();
